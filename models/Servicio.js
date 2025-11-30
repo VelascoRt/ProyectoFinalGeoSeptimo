eb5ced4e3ae2732d27b2a62eff7bca1207d8f54f
VelascoRt <velascogmac@gmail.com>
2025-11-29 21:08:54 -0600

    BackEnd Changes

--- models/Servicio.js
+++ models/Servicio.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
 const ServicioSchema = new mongoose.Schema({
-    id: {
-        type: Number,
-        unique: true
-    },
     nombre: {
         type: String,
         required: true
